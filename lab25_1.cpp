f40fdfd31216954b678b6c186b799e3168b2ebcc
= <=>
2023-03-13 20:03:36 +0700

    graduated

--- lab25_1.cpp
+++ lab25_1.cpp
@@ -63,4 +63,18 @@ void List::append(int d){
 	size++;
 }
 
-//Write List::remove() here
+void List::remove(int d){
+	Node *current = root;
+	if(d == 0) {
+		root = root->next;
+		delete current;
+	}else{
+		for(int i = 0;i < d-1;i++) {
+			current = current->next;
+		}
+		Node *temp = current->next;
+		current->next = temp->next;
+		delete temp;
+	}
+	size--;
+}
